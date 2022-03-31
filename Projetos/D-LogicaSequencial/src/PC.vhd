b2f6ba2458a6cf8c63362ea558717e2922c1d75c
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-03-31 00:19:39 -0300

    Update PC.vhd

--- Projetos/D-LogicaSequencial/src/PC.vhd
+++ Projetos/D-LogicaSequencial/src/PC.vhd
@@ -9,6 +9,8 @@
 -- else if (inc[t] == 1) out[t+1] = out[t] + 1
 -- else out[t+1] = out[t]
 
+-- O program counter será o nosso endereçador de memória da CPU, ele será responsável por apontar para a próxima instrução a ser executada. Como normalmente um código segue um fluxo sequencial (uma linha na sequência da outra) o PC possui a habilidade de se auto incrementar a cada clock (apontando assim para a próxima instrução), mas ele tem que suportar condições (if, while, ...) rompendo com esse fluxo contínuo.
+
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL;
@@ -46,5 +48,31 @@ architecture arch of PC is
 
 begin
 
+    Inc16 inc16(
+        a => muxOut,
+        q => muxOut
+    );
+
+    Register16 reg16(
+        clock => clock,
+        input => input,
+        load => load,
+        output => muxOut
+    );
+
+    process(clock)
+    begin
+        if rising_edge(clock) then
+            if reset = '1' then
+                muxOut <= (others => '0');
+            elsif load = '1' then
+                muxOut <= input;
+            elsif increment = '1' then
+                muxOut <= muxOut + (others => '1');
+            end if;
+        end if;
+    end process;
+
+    muxOut <= reg16.output;
 
 end architecture;
